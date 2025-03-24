fd1a543a9d6cad1f1c0d40f82ad6681b0389c6fe
ilsong963 <ilsong963@naver.com>
2025-03-24 15:34:06 +0900

    feat: menu 추가

--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -5,6 +5,22 @@ class RecipeMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("RecipeMenu");
+    return Row(
+      children: [
+        Container(
+          width: 60,
+          height: 80,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), border: Border.all(color: Colors.black12)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.food_bank, color: Colors.redAccent, size: 30),
+              SizedBox(height: 5),
+              Text('ALL', style: TextStyle(color: Colors.black87)),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
