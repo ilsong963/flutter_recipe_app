d6cb5bcede0d181b2d5b64b6dbe81b5f4f94c80b
ilsong963 <ilsong963@naver.com>
2025-03-24 15:37:50 +0900

    feat: menuItem 여백

--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -5,22 +5,35 @@ class RecipeMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 60,
-          height: 80,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), border: Border.all(color: Colors.black12)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.food_bank, color: Colors.redAccent, size: 30),
-              SizedBox(height: 5),
-              Text('ALL', style: TextStyle(color: Colors.black87)),
-            ],
-          ),
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Row(
+        children: [
+          menuItem(Icons.food_bank, 'ALL'),
+          SizedBox(width: 25),
+          menuItem(Icons.food_bank, 'Coffee'),
+          SizedBox(width: 25),
+          menuItem(Icons.food_bank, 'Burger'),
+          SizedBox(width: 25),
+          menuItem(Icons.food_bank, 'Pizza'),
+        ],
+      ),
+    );
+  }
+
+  Container menuItem(IconData iconData, String text) {
+    return Container(
+      width: 60,
+      height: 80,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), border: Border.all(color: Colors.black12)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(iconData, color: Colors.redAccent, size: 30),
+          SizedBox(height: 5),
+          Text(text, style: TextStyle(color: Colors.black87)),
+        ],
+      ),
     );
   }
 }
