76652e430824d97ae481a37eb7c2eb9e436efa82
ilsong963 <ilsong963@naver.com>
2025-03-24 14:30:14 +0900

    feat: appbar추가

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,19 @@ class RecipePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(icon: const Icon(Icons.navigate_next), tooltip: 'Go to the next page', onPressed: () {}),
+        ],
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
